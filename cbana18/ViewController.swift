c948b1e7433f8a753d109453b2a2969b292b2d51
laimetal <salaihute@gmail.com>
2017-12-23 22:51:06 -0500

    Created CBANA project

--- cbana18/ViewController.swift
+++ cbana18/ViewController.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var Username: UITextField!
+    @IBOutlet weak var Password: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,37 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func CreateAccount(_ sender: Any) {
+        
+        Auth.auth().createUser(withEmail: Username.text!, password: Password.text!, completion: { (user, error) in
+            
+            if error != nil{
+                self.login()
+                
+                }
+            else{
+                
+                print("User Created")
+                self.login()
+            }
+            })
+    }
+    func login(){
+        Auth.auth().signIn(withEmail: Username.text!, password: Password.text!) { (user, error) in
+            
+            if error != nil{
+               print("Incorrect")
+                
+            }
+            else{
+                print("Good Job")
+            }
+        }
+        
+        
+        
+        
+    }
+    
 }
 
